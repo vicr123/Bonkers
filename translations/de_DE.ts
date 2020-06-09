0cee3ca4938480a7cf9955887e16722724d10d78
Adalbert Šarić <saric.adalbert349@gmail.com>
2020-06-09 16:56:48 +0000

    Translated using Weblate (German)
    Currently translated at 96.9% (32 of 33 strings)
    
    Translation: Bonkers/Bonkers Desktop
    Translate-URL: https://translate.vicr123.com/projects/bonkers/bonkers-desktop/de/

--- translations/de_DE.ts
+++ translations/de_DE.ts
@@ -11,37 +11,37 @@
     <message>
         <location filename="../crasheddialog.ui" line="112"/>
         <source>Start the application again</source>
-        <translation type="unfinished"></translation>
+        <translation>Anwendung erneut öffnen</translation>
     </message>
     <message>
         <location filename="../crasheddialog.ui" line="132"/>
         <source>Stop running the application</source>
-        <translation type="unfinished"></translation>
+        <translation>Anwendung stoppen</translation>
     </message>
     <message>
         <location filename="../crasheddialog.ui" line="152"/>
         <source>More Information</source>
-        <translation type="unfinished"></translation>
+        <translation>Weitere Informationen</translation>
     </message>
     <message>
         <location filename="../crasheddialog.ui" line="165"/>
         <source>Show more information about the problem</source>
-        <translation type="unfinished"></translation>
+        <translation>Weitere Informationen über dieses Problem anzeigen</translation>
     </message>
     <message>
         <location filename="../crasheddialog.ui" line="172"/>
         <source>File Bug</source>
-        <translation type="unfinished"></translation>
+        <translation>Bug melden</translation>
     </message>
     <message>
         <location filename="../crasheddialog.ui" line="185"/>
         <source>File a bug report to the developer</source>
-        <translation type="unfinished"></translation>
+        <translation>Einen Bug an den Entwickler melden</translation>
     </message>
     <message>
         <location filename="../crasheddialog.ui" line="247"/>
         <source>Information about this problem</source>
-        <translation type="unfinished"></translation>
+        <translation>Informationen über dieses Problem</translation>
     </message>
     <message>
         <location filename="../crasheddialog.ui" line="292"/>
@@ -50,27 +50,31 @@
 When reporting a bug, if any debugging information is available, please attach it to the bug report.
 
 It&apos;s a good idea to check through the contents of the available debugging information here for any personal information and redact it before you attach it to a bug report.</source>
-        <translation type="unfinished"></translation>
+        <translation>Diese Debugging-Informationen können dem Entwickler helfen, das Problem zu beheben. Sie können die Debugging-Informationen in einer Datei für spätere Gebräuche speichern, oder am Bugreport anhängen.
+
+Soll ein Bug gemeldet werden und es sind Debugging-Informationen verfügbar, wird darum gebeten, diese Informationen im Bugreport anzuhängen.
+
+Es ist ideal, die Informationen durchzulesen und, falls vorhanden, jedwede persönlichen Details aus diesen Informationen zu exkludieren, bevor die Informationen angehängt werden.</translation>
     </message>
     <message>
         <location filename="../crasheddialog.cpp" line="44"/>
         <source>Sorry, %1 had a problem and has stopped working.</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 hatte ein Problem und hat sich aufgehängt. Wir bitten um Entschuldigung.</translation>
     </message>
     <message>
         <location filename="../crasheddialog.cpp" line="45"/>
         <source>Relaunch %1</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 erneut öffnen</translation>
     </message>
     <message>
         <location filename="../crasheddialog.cpp" line="46"/>
         <source>Exit %1</source>
-        <translation type="unfinished"></translation>
+        <translation>1% beenden</translation>
     </message>
     <message>
         <location filename="../crasheddialog.cpp" line="134"/>
         <source>Text File</source>
-        <translation type="unfinished"></translation>
+        <translation>Textdatei</translation>
     </message>
 </context>
 <context>
@@ -78,12 +82,12 @@ It&apos;s a good idea to check through the contents of the available debugging i
     <message>
         <location filename="../vicr123filebugwidget/totpcoderequest.ui" line="71"/>
         <source>Two Factor Authentication</source>
-        <translation type="unfinished"></translation>
+        <translation>Zwei-Faktor-Authentifizierung</translation>
     </message>
     <message>
         <location filename="../vicr123filebugwidget/totpcoderequest.ui" line="113"/>
         <source>Obtain a verification code to verify your identity by opening the Two Factor Authentication app on your phone.</source>
-        <translation type="unfinished"></translation>
+        <translation>Erhalten Sie einen Verifizierungscode, um Ihre Identität zu verifizieren, indem sie die Zwei-Faktor-Authentifizierungs-App auf ihrem Smartphone öffnen.</translation>
     </message>
 </context>
 <context>
@@ -91,99 +95,101 @@ It&apos;s a good idea to check through the contents of the available debugging i
     <message>
         <location filename="../vicr123filebugwidget/vicr123filebugwidget.ui" line="78"/>
         <source>File Bug</source>
-        <translation type="unfinished"></translation>
+        <translation>Bug melden</translation>
     </message>
     <message>
         <location filename="../vicr123filebugwidget/vicr123filebugwidget.ui" line="103"/>
         <source>Thanks for offering to report this bug. We&apos;ll just need you to answer a few questions, and then we&apos;ll be on our way!
 
 Since the developer may need to ask further questions, you&apos;ll be asked to log into or create an account on the vicr123 bugtracking system as part of the bug reporting process. If you do not want us to contact you, you may back out of the bug reporting process at any time.</source>
-        <translation type="unfinished"></translation>
+        <translation>Vielen Dank für die Bugreport-Anfrage. Wir möchten noch, dass Sie einige Fragen von uns beantworten, sodass wir anschließend mit Elan loslegen können!
+
+Da der Entwickler darauf besteht, für den Zweck des Bugreports einige weitere Fragen zu stellen, werden Sie beauftragt, sich am vicr123-Bug-Tracking-System ein Konto anzulegen bzw. sich anzumelden, falls Sie bereits eines haben. Sollten wir Sie bei Bedarf nicht kontaktieren, können Sie jederzeit vom Fehlerberichterstattungsprozess zurücktreten.</translation>
     </message>
     <message>
         <location filename="../vicr123filebugwidget/vicr123filebugwidget.ui" line="154"/>
         <source>REPRODUCIBILITY</source>
-        <translation type="unfinished"></translation>
+        <translation>REPRODUZIBILITÄT</translation>
     </message>
     <message>
         <location filename="../vicr123filebugwidget/vicr123filebugwidget.ui" line="161"/>
         <source>Are you able to tell us what you did when the app stopped working?</source>
-        <translation type="unfinished"></translation>
+        <translation>Sind Sie in der Lage, uns zu erzählen, was Sie gemacht haben, als die Anwendung sich aufhing?</translation>
     </message>
     <message>
         <location filename="../vicr123filebugwidget/vicr123filebugwidget.ui" line="183"/>
         <location filename="../vicr123filebugwidget/vicr123filebugwidget.ui" line="238"/>
         <source>Yes</source>
-        <translation type="unfinished"></translation>
+        <translation>Ja</translation>
     </message>
     <message>
         <location filename="../vicr123filebugwidget/vicr123filebugwidget.ui" line="190"/>
         <location filename="../vicr123filebugwidget/vicr123filebugwidget.ui" line="245"/>
         <source>No</source>
-        <translation type="unfinished"></translation>
+        <translation>Nein</translation>
     </message>
     <message>
         <location filename="../vicr123filebugwidget/vicr123filebugwidget.ui" line="216"/>
         <source>If you perform the same steps, does the problem recur?</source>
-        <translation type="unfinished"></translation>
+        <translation>Wenn Sie dieselben Schritte nochmal ausführen, tritt das Problem erneut auf?</translation>
     </message>
     <message>
         <location filename="../vicr123filebugwidget/vicr123filebugwidget.ui" line="255"/>
         <source>Intermittently</source>
-        <translation type="unfinished"></translation>
+        <translation>Intermittiert</translation>
     </message>
     <message>
         <location filename="../vicr123filebugwidget/vicr123filebugwidget.ui" line="293"/>
         <source>Next Page</source>
-        <translation type="unfinished"></translation>
+        <translation>Nächste Seite</translation>
     </message>
     <message>
         <location filename="../vicr123filebugwidget/vicr123filebugwidget.ui" line="379"/>
         <location filename="../vicr123filebugwidget/vicr123filebugwidget.ui" line="469"/>
         <source>Log In</source>
-        <translation type="unfinished"></translation>
+        <translation>Anmelden</translation>
     </message>
     <message>
         <location filename="../vicr123filebugwidget/vicr123filebugwidget.ui" line="421"/>
         <source>Log into the vicr123 Bug Reporting System using your username and password. If you don&apos;t have an account, you can register for one at &lt;a href=&quot;https://bugs.vicr123.com/&quot;&gt;bugs.vicr123.com&lt;/a&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>Melden Sie sich am vicr123-Bug-Tracking-System mit Ihrem Benutzernamen und Passwort an. Sollten Sie kein Konto haben, können Sie sich auf &lt;a href=&quot;https://bugs.vicr123.com/&quot;&gt;bugs.vicr123.com&lt;/a&gt; registrieren</translation>
     </message>
     <message>
         <location filename="../vicr123filebugwidget/vicr123filebugwidget.ui" line="434"/>
         <source>Username</source>
-        <translation type="unfinished"></translation>
+        <translation>Benutzername</translation>
     </message>
     <message>
         <location filename="../vicr123filebugwidget/vicr123filebugwidget.ui" line="444"/>
         <source>Password</source>
-        <translation type="unfinished"></translation>
+        <translation>Passwort</translation>
     </message>
     <message>
         <location filename="../vicr123filebugwidget/vicr123filebugwidget.cpp" line="125"/>
         <source>Logged in!</source>
-        <translation type="unfinished"></translation>
+        <translation>Sie sind angemeldet!</translation>
     </message>
     <message>
         <location filename="../vicr123filebugwidget/vicr123filebugwidget.cpp" line="126"/>
         <location filename="../vicr123filebugwidget/vicr123filebugwidget.cpp" line="149"/>
         <source>Correct your password and try again.</source>
-        <translation type="unfinished"></translation>
+        <translation>Korrigieren Sie ihr Passwort und versuchen Sie es anschließend erneut.</translation>
     </message>
     <message>
         <location filename="../vicr123filebugwidget/vicr123filebugwidget.cpp" line="148"/>
         <source>Incorrect Password</source>
-        <translation type="unfinished"></translation>
+        <translation>Ihr Passwort ist inkorrekt</translation>
     </message>
     <message>
         <location filename="../vicr123filebugwidget/vicr123filebugwidget.cpp" line="155"/>
         <location filename="../vicr123filebugwidget/vicr123filebugwidget.cpp" line="171"/>
         <source>Something went wrong</source>
-        <translation type="unfinished"></translation>
+        <translation>Etwas ist schiefgelaufen</translation>
     </message>
     <message>
         <location filename="../vicr123filebugwidget/vicr123filebugwidget.cpp" line="156"/>
         <source>Couldn&apos;t get to the bug reporting server.</source>
-        <translation type="unfinished"></translation>
+        <translation>Die Verbindung zum Bugreporting-Server ist fehlgeschlagen.</translation>
     </message>
 </context>
 </TS>

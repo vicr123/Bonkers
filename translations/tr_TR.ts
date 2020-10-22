3f2937b59157f0d42437d94ee5643ae63dde7a73
Atahan Reis <atahan.r@outlook.com>
2020-10-22 01:23:28 +0000

    Translated using Weblate (Turkish)
    Currently translated at 100.0% (33 of 33 strings)
    
    Translation: Bonkers/Bonkers Desktop
    Translate-URL: https://translate.vicr123.com/projects/bonkers/bonkers-desktop/tr/

--- translations/tr_TR.ts
+++ translations/tr_TR.ts
@@ -6,42 +6,42 @@
     <message>
         <location filename="../crasheddialog.ui" line="14"/>
         <source>Oh, Bonkers!</source>
-        <translation type="unfinished"></translation>
+        <translation>Oh, Bir Kaçık!</translation>
     </message>
     <message>
         <location filename="../crasheddialog.ui" line="112"/>
         <source>Start the application again</source>
-        <translation type="unfinished"></translation>
+        <translation>Uygulamayı yeniden başlatın</translation>
     </message>
     <message>
         <location filename="../crasheddialog.ui" line="132"/>
         <source>Stop running the application</source>
-        <translation type="unfinished"></translation>
+        <translation>Uygulamayı çalıştırmayı durdurun</translation>
     </message>
     <message>
         <location filename="../crasheddialog.ui" line="152"/>
         <source>More Information</source>
-        <translation type="unfinished"></translation>
+        <translation>Daha fazla bilgi</translation>
     </message>
     <message>
         <location filename="../crasheddialog.ui" line="165"/>
         <source>Show more information about the problem</source>
-        <translation type="unfinished"></translation>
+        <translation>Sorunla ilgili daha fazla bilgi göster</translation>
     </message>
     <message>
         <location filename="../crasheddialog.ui" line="172"/>
         <source>File Bug</source>
-        <translation type="unfinished"></translation>
+        <translation>Dosya Hatası</translation>
     </message>
     <message>
         <location filename="../crasheddialog.ui" line="185"/>
         <source>File a bug report to the developer</source>
-        <translation type="unfinished"></translation>
+        <translation>Geliştiriciye bir hata raporu gönderin</translation>
     </message>
     <message>
         <location filename="../crasheddialog.ui" line="247"/>
         <source>Information about this problem</source>
-        <translation type="unfinished"></translation>
+        <translation>Bu problemle ilgili bilgiler</translation>
     </message>
     <message>
         <location filename="../crasheddialog.ui" line="292"/>
@@ -50,27 +50,31 @@
 When reporting a bug, if any debugging information is available, please attach it to the bug report.
 
 It&apos;s a good idea to check through the contents of the available debugging information here for any personal information and redact it before you attach it to a bug report.</source>
-        <translation type="unfinished"></translation>
+        <translation>Bu hata ayıklama bilgileri, geliştiricinin bu sorunu gidermesine yardımcı olabilir. Bu hata ayıklama bilgilerini daha sonra başvurmak üzere bir dosyaya kaydedebilir veya bir hata raporuna ekleyebilirsiniz.
+
+Bir hata bildirirken, herhangi bir hata ayıklama bilgisi varsa, lütfen hata raporuna ekleyin.
+
+Herhangi bir kişisel bilgi için mevcut hata ayıklama bilgilerinin içeriğini kontrol etmek ve bir hata raporuna eklemeden önce düzenlemek iyi bir fikirdir.</translation>
     </message>
     <message>
         <location filename="../crasheddialog.cpp" line="44"/>
         <source>Sorry, %1 had a problem and has stopped working.</source>
-        <translation type="unfinished"></translation>
+        <translation>Üzgünüm, %1 bir sorun yaşadı ve çalışmayı durdurdu.</translation>
     </message>
     <message>
         <location filename="../crasheddialog.cpp" line="45"/>
         <source>Relaunch %1</source>
-        <translation type="unfinished"></translation>
+        <translation>%1&apos;i yeniden başlatın</translation>
     </message>
     <message>
         <location filename="../crasheddialog.cpp" line="46"/>
         <source>Exit %1</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 çık</translation>
     </message>
     <message>
         <location filename="../crasheddialog.cpp" line="134"/>
         <source>Text File</source>
-        <translation type="unfinished"></translation>
+        <translation>Metin dosyası</translation>
     </message>
 </context>
 <context>
@@ -78,12 +82,12 @@ It&apos;s a good idea to check through the contents of the available debugging i
     <message>
         <location filename="../vicr123filebugwidget/totpcoderequest.ui" line="71"/>
         <source>Two Factor Authentication</source>
-        <translation type="unfinished"></translation>
+        <translation>İki Faktörlü Kimlik Doğrulama</translation>
     </message>
     <message>
         <location filename="../vicr123filebugwidget/totpcoderequest.ui" line="113"/>
         <source>Obtain a verification code to verify your identity by opening the Two Factor Authentication app on your phone.</source>
-        <translation type="unfinished"></translation>
+        <translation>Telefonunuzda iki faktörlü kimlik doğrulama uygulamasını açarak kimliğinizi doğrulamak için bir doğrulama kodu edinin.</translation>
     </message>
 </context>
 <context>
@@ -91,99 +95,101 @@ It&apos;s a good idea to check through the contents of the available debugging i
     <message>
         <location filename="../vicr123filebugwidget/vicr123filebugwidget.ui" line="78"/>
         <source>File Bug</source>
-        <translation type="unfinished"></translation>
+        <translation>Dosya Hatası</translation>
     </message>
     <message>
         <location filename="../vicr123filebugwidget/vicr123filebugwidget.ui" line="103"/>
         <source>Thanks for offering to report this bug. We&apos;ll just need you to answer a few questions, and then we&apos;ll be on our way!
 
 Since the developer may need to ask further questions, you&apos;ll be asked to log into or create an account on the vicr123 bugtracking system as part of the bug reporting process. If you do not want us to contact you, you may back out of the bug reporting process at any time.</source>
-        <translation type="unfinished"></translation>
+        <translation>Bu hatayı bildirmeyi teklif ettiğiniz için teşekkür ederiz. Sadece birkaç soruya cevap vermeni istiyoruz, ve sonra yolumuza devam edeceğiz!
+
+Geliştiricinin başka sorular sorması gerekebileceğinden, hata raporlama sürecinin bir parçası olarak vicr123 bugtracking sisteminde oturum açmanız veya bir hesap oluşturmanız istenir. Sizinle iletişim kurmamızı istemiyorsanız, istediğiniz zaman hata raporlama sürecinden geri dönebilirsiniz.</translation>
     </message>
     <message>
         <location filename="../vicr123filebugwidget/vicr123filebugwidget.ui" line="154"/>
         <source>REPRODUCIBILITY</source>
-        <translation type="unfinished"></translation>
+        <translation>TEKRARLANABİLİLLİK</translation>
     </message>
     <message>
         <location filename="../vicr123filebugwidget/vicr123filebugwidget.ui" line="161"/>
         <source>Are you able to tell us what you did when the app stopped working?</source>
-        <translation type="unfinished"></translation>
+        <translation>Uygulama çalışmayı durdurduğunda ne yaptığınızı bize söyleyebilir misiniz?</translation>
     </message>
     <message>
         <location filename="../vicr123filebugwidget/vicr123filebugwidget.ui" line="183"/>
         <location filename="../vicr123filebugwidget/vicr123filebugwidget.ui" line="238"/>
         <source>Yes</source>
-        <translation type="unfinished"></translation>
+        <translation>Evet</translation>
     </message>
     <message>
         <location filename="../vicr123filebugwidget/vicr123filebugwidget.ui" line="190"/>
         <location filename="../vicr123filebugwidget/vicr123filebugwidget.ui" line="245"/>
         <source>No</source>
-        <translation type="unfinished"></translation>
+        <translation>Yok</translation>
     </message>
     <message>
         <location filename="../vicr123filebugwidget/vicr123filebugwidget.ui" line="216"/>
         <source>If you perform the same steps, does the problem recur?</source>
-        <translation type="unfinished"></translation>
+        <translation>Aynı adımları gerçekleştirirseniz, sorun yineleniyor mu?</translation>
     </message>
     <message>
         <location filename="../vicr123filebugwidget/vicr123filebugwidget.ui" line="255"/>
         <source>Intermittently</source>
-        <translation type="unfinished"></translation>
+        <translation>Aralıklı olarak</translation>
     </message>
     <message>
         <location filename="../vicr123filebugwidget/vicr123filebugwidget.ui" line="293"/>
         <source>Next Page</source>
-        <translation type="unfinished"></translation>
+        <translation>Sonraki Sayfa</translation>
     </message>
     <message>
         <location filename="../vicr123filebugwidget/vicr123filebugwidget.ui" line="379"/>
         <location filename="../vicr123filebugwidget/vicr123filebugwidget.ui" line="469"/>
         <source>Log In</source>
-        <translation type="unfinished"></translation>
+        <translation>Oturum aç</translation>
     </message>
     <message>
         <location filename="../vicr123filebugwidget/vicr123filebugwidget.ui" line="421"/>
         <source>Log into the vicr123 Bug Reporting System using your username and password. If you don&apos;t have an account, you can register for one at &lt;a href=&quot;https://bugs.vicr123.com/&quot;&gt;bugs.vicr123.com&lt;/a&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>Kullanıcı adınızı ve şifrenizi kullanarak vicr123 Hata Raporlama Sistemine giriş yapın. Bir hesabınız yoksa, &lt;a href=&quot;https://bugs.vicr123.com/&quot;&gt; bugs.vicr123.com &lt;/a&gt; adresinden kayıt olabilirsiniz.</translation>
     </message>
     <message>
         <location filename="../vicr123filebugwidget/vicr123filebugwidget.ui" line="434"/>
         <source>Username</source>
-        <translation type="unfinished"></translation>
+        <translation>Kullanıcı</translation>
     </message>
     <message>
         <location filename="../vicr123filebugwidget/vicr123filebugwidget.ui" line="444"/>
         <source>Password</source>
-        <translation type="unfinished"></translation>
+        <translation>Şifre</translation>
     </message>
     <message>
         <location filename="../vicr123filebugwidget/vicr123filebugwidget.cpp" line="125"/>
         <source>Logged in!</source>
-        <translation type="unfinished"></translation>
+        <translation>Giriş!</translation>
     </message>
     <message>
         <location filename="../vicr123filebugwidget/vicr123filebugwidget.cpp" line="126"/>
         <location filename="../vicr123filebugwidget/vicr123filebugwidget.cpp" line="149"/>
         <source>Correct your password and try again.</source>
-        <translation type="unfinished"></translation>
+        <translation>Parolanızı düzeltin ve yeniden deneyin.</translation>
     </message>
     <message>
         <location filename="../vicr123filebugwidget/vicr123filebugwidget.cpp" line="148"/>
         <source>Incorrect Password</source>
-        <translation type="unfinished"></translation>
+        <translation>Yanlış Şifre</translation>
     </message>
     <message>
         <location filename="../vicr123filebugwidget/vicr123filebugwidget.cpp" line="155"/>
         <location filename="../vicr123filebugwidget/vicr123filebugwidget.cpp" line="171"/>
         <source>Something went wrong</source>
-        <translation type="unfinished"></translation>
+        <translation>Bir şeyler yanlış gitti</translation>
     </message>
     <message>
         <location filename="../vicr123filebugwidget/vicr123filebugwidget.cpp" line="156"/>
         <source>Couldn&apos;t get to the bug reporting server.</source>
-        <translation type="unfinished"></translation>
+        <translation>Hata raporlama sunucusuna ulaşılamadı.</translation>
     </message>
 </context>
 </TS>

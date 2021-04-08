5d11feee616df575908c4d680ae7d367b2d3d7e3
JPlexer <jplexer.at.wcg@gmail.com>
2021-04-08 10:02:36 +0000

    Translated using Weblate (German)
    Currently translated at 100.0% (33 of 33 strings)
    
    Translation: Bonkers/Bonkers Desktop
    Translate-URL: https://translate.vicr123.com/projects/bonkers/bonkers-desktop/de/

--- translations/de_DE.ts
+++ translations/de_DE.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="../crasheddialog.ui" line="14"/>
         <source>Oh, Bonkers!</source>
-        <translation type="unfinished"></translation>
+        <translation>Oh, verflixt!</translation>
     </message>
     <message>
         <location filename="../crasheddialog.ui" line="112"/>

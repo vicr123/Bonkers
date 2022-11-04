1c297cd57a7ed14f0e542dd85037ac754841ba48
sebosh <sebastian.svec@outlook.com>
2022-11-04 17:01:58 +0000

    Translated using Weblate (Czech)
    Currently translated at 96.9% (32 of 33 strings)
    
    Translation: Bonkers/Bonkers Desktop
    Translate-URL: https://translate.vicr123.com/projects/bonkers/bonkers-desktop/cs/

--- translations/cs_CZ.ts
+++ translations/cs_CZ.ts
@@ -1,47 +1,47 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" sourcelanguage="en_US">
+<TS version="2.1" sourcelanguage="en_US" language="cs_CZ">
 <context>
     <name>CrashedDialog</name>
     <message>
         <location filename="../crasheddialog.ui" line="14"/>
         <source>Oh, Bonkers!</source>
-        <translation type="unfinished"></translation>
+        <translation>Jejda!</translation>
     </message>
     <message>
         <location filename="../crasheddialog.ui" line="112"/>
         <source>Start the application again</source>
-        <translation type="unfinished"></translation>
+        <translation>Spustit aplikaci znovu</translation>
     </message>
     <message>
         <location filename="../crasheddialog.ui" line="132"/>
         <source>Stop running the application</source>
-        <translation type="unfinished"></translation>
+        <translation>Ukončit aplikaci</translation>
     </message>
     <message>
         <location filename="../crasheddialog.ui" line="152"/>
         <source>More Information</source>
-        <translation type="unfinished"></translation>
+        <translation>Více informací</translation>
     </message>
     <message>
         <location filename="../crasheddialog.ui" line="165"/>
         <source>Show more information about the problem</source>
-        <translation type="unfinished"></translation>
+        <translation>Zobrazit více informací o problému</translation>
     </message>
     <message>
         <location filename="../crasheddialog.ui" line="172"/>
         <source>File Bug</source>
-        <translation type="unfinished"></translation>
+        <translation>Nahlásit chybu</translation>
     </message>
     <message>
         <location filename="../crasheddialog.ui" line="185"/>
         <source>File a bug report to the developer</source>
-        <translation type="unfinished"></translation>
+        <translation>Nahlásit chybu vývojářovi</translation>
     </message>
     <message>
         <location filename="../crasheddialog.ui" line="247"/>
         <source>Information about this problem</source>
-        <translation type="unfinished"></translation>
+        <translation>Informace o problému</translation>
     </message>
     <message>
         <location filename="../crasheddialog.ui" line="292"/>
@@ -50,27 +50,31 @@
 When reporting a bug, if any debugging information is available, please attach it to the bug report.
 
 It&apos;s a good idea to check through the contents of the available debugging information here for any personal information and redact it before you attach it to a bug report.</source>
-        <translation type="unfinished"></translation>
+        <translation>Tyto debugovací informace mohou pomoci vývojářovi ve spravování tohoto problému. Můžete si uložit tyto informace do souboru pro pozdější použití anebo pro přiložení ke hlášení chyby.
+
+Když nahlašujete chybu, pokud jsou dostupné jakékoli debugovací informace, prosím přiložte je ke hlášení.
+
+Doporučujeme zkontrolovat obsah debugovacích informací a smazat citlivé detaily před přiložením ke hlášení.</translation>
     </message>
     <message>
         <location filename="../crasheddialog.cpp" line="44"/>
         <source>Sorry, %1 had a problem and has stopped working.</source>
-        <translation type="unfinished"></translation>
+        <translation>Omlouvám se, v %1 nastala chyba a ukončila jej.</translation>
     </message>
     <message>
         <location filename="../crasheddialog.cpp" line="45"/>
         <source>Relaunch %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Spustit %1 znovu</translation>
     </message>
     <message>
         <location filename="../crasheddialog.cpp" line="46"/>
         <source>Exit %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Ukončit %1</translation>
     </message>
     <message>
         <location filename="../crasheddialog.cpp" line="134"/>
         <source>Text File</source>
-        <translation type="unfinished"></translation>
+        <translation>Textový soubor</translation>
     </message>
 </context>
 <context>
@@ -78,12 +82,12 @@ It&apos;s a good idea to check through the contents of the available debugging i
     <message>
         <location filename="../vicr123filebugwidget/totpcoderequest.ui" line="71"/>
         <source>Two Factor Authentication</source>
-        <translation type="unfinished"></translation>
+        <translation>Dvoufaktorové ověření</translation>
     </message>
     <message>
         <location filename="../vicr123filebugwidget/totpcoderequest.ui" line="113"/>
         <source>Obtain a verification code to verify your identity by opening the Two Factor Authentication app on your phone.</source>
-        <translation type="unfinished"></translation>
+        <translation>Získejte ověřovací kód pro ověření vaší identity otevřením vaší aplikace pro dvoufaktorové ověření na telefonu.</translation>
     </message>
 </context>
 <context>
@@ -91,99 +95,101 @@ It&apos;s a good idea to check through the contents of the available debugging i
     <message>
         <location filename="../vicr123filebugwidget/vicr123filebugwidget.ui" line="78"/>
         <source>File Bug</source>
-        <translation type="unfinished"></translation>
+        <translation>Nahlásit chybu</translation>
     </message>
     <message>
         <location filename="../vicr123filebugwidget/vicr123filebugwidget.ui" line="103"/>
         <source>Thanks for offering to report this bug. We&apos;ll just need you to answer a few questions, and then we&apos;ll be on our way!
 
 Since the developer may need to ask further questions, you&apos;ll be asked to log into or create an account on the vicr123 bugtracking system as part of the bug reporting process. If you do not want us to contact you, you may back out of the bug reporting process at any time.</source>
-        <translation type="unfinished"></translation>
+        <translation>Děkujeme, že chcete nahlásit tuto chybu. Budeme jen potřebovat, abyste odpověděli na pár otázek a hned se pustíme do práce!
+
+Vzhledem k tomu, že se možná vývojář bude potřebovat zeptat na další informace, budete vyzváni k přihlášení nebo vytvoření účtu na vicr123 systému pro hlášení chyb jako součást procesu hlášení chyby. Pokud nechcete být kontaktováni, můžete kdykoli vycouvat z hlášení chyby.</translation>
     </message>
     <message>
         <location filename="../vicr123filebugwidget/vicr123filebugwidget.ui" line="154"/>
         <source>REPRODUCIBILITY</source>
-        <translation type="unfinished"></translation>
+        <translation>KROKY K REPRODUKCI CHYBY</translation>
     </message>
     <message>
         <location filename="../vicr123filebugwidget/vicr123filebugwidget.ui" line="161"/>
         <source>Are you able to tell us what you did when the app stopped working?</source>
-        <translation type="unfinished"></translation>
+        <translation>Můžete nám říct, co jste dělal/a když aplikace přestala fungovat?</translation>
     </message>
     <message>
         <location filename="../vicr123filebugwidget/vicr123filebugwidget.ui" line="183"/>
         <location filename="../vicr123filebugwidget/vicr123filebugwidget.ui" line="238"/>
         <source>Yes</source>
-        <translation type="unfinished"></translation>
+        <translation>Ano</translation>
     </message>
     <message>
         <location filename="../vicr123filebugwidget/vicr123filebugwidget.ui" line="190"/>
         <location filename="../vicr123filebugwidget/vicr123filebugwidget.ui" line="245"/>
         <source>No</source>
-        <translation type="unfinished"></translation>
+        <translation>Ne</translation>
     </message>
     <message>
         <location filename="../vicr123filebugwidget/vicr123filebugwidget.ui" line="216"/>
         <source>If you perform the same steps, does the problem recur?</source>
-        <translation type="unfinished"></translation>
+        <translation>Pokud provedete stejné kroky, nastane problém znovu?</translation>
     </message>
     <message>
         <location filename="../vicr123filebugwidget/vicr123filebugwidget.ui" line="255"/>
         <source>Intermittently</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../vicr123filebugwidget/vicr123filebugwidget.ui" line="293"/>
         <source>Next Page</source>
-        <translation type="unfinished"></translation>
+        <translation>Následující stránka</translation>
     </message>
     <message>
         <location filename="../vicr123filebugwidget/vicr123filebugwidget.ui" line="379"/>
         <location filename="../vicr123filebugwidget/vicr123filebugwidget.ui" line="469"/>
         <source>Log In</source>
-        <translation type="unfinished"></translation>
+        <translation>Přihlásit se</translation>
     </message>
     <message>
         <location filename="../vicr123filebugwidget/vicr123filebugwidget.ui" line="421"/>
         <source>Log into the vicr123 Bug Reporting System using your username and password. If you don&apos;t have an account, you can register for one at &lt;a href=&quot;https://bugs.vicr123.com/&quot;&gt;bugs.vicr123.com&lt;/a&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>Přihlašte se do vicr123 systému pro hlášení chyb pomocí svého jména a hesla. Pokud nemáte účet, můžete se zaregistrovat na &lt;a href=&quot;https://bugs.vicr123.com/&quot;&gt;bugs.vicr123.com&lt;/a&gt;</translation>
     </message>
     <message>
         <location filename="../vicr123filebugwidget/vicr123filebugwidget.ui" line="434"/>
         <source>Username</source>
-        <translation type="unfinished"></translation>
+        <translation>Uživatelské jméno</translation>
     </message>
     <message>
         <location filename="../vicr123filebugwidget/vicr123filebugwidget.ui" line="444"/>
         <source>Password</source>
-        <translation type="unfinished"></translation>
+        <translation>Heslo</translation>
     </message>
     <message>
         <location filename="../vicr123filebugwidget/vicr123filebugwidget.cpp" line="125"/>
         <source>Logged in!</source>
-        <translation type="unfinished"></translation>
+        <translation>Úspěšně přihlášeno!</translation>
     </message>
     <message>
         <location filename="../vicr123filebugwidget/vicr123filebugwidget.cpp" line="126"/>
         <location filename="../vicr123filebugwidget/vicr123filebugwidget.cpp" line="149"/>
         <source>Correct your password and try again.</source>
-        <translation type="unfinished"></translation>
+        <translation>Opravte své heslo a zkuste znovu.</translation>
     </message>
     <message>
         <location filename="../vicr123filebugwidget/vicr123filebugwidget.cpp" line="148"/>
         <source>Incorrect Password</source>
-        <translation type="unfinished"></translation>
+        <translation>Nesprávné heslo</translation>
     </message>
     <message>
         <location filename="../vicr123filebugwidget/vicr123filebugwidget.cpp" line="155"/>
         <location filename="../vicr123filebugwidget/vicr123filebugwidget.cpp" line="171"/>
         <source>Something went wrong</source>
-        <translation type="unfinished"></translation>
+        <translation>Něco se pokazilo</translation>
     </message>
     <message>
         <location filename="../vicr123filebugwidget/vicr123filebugwidget.cpp" line="156"/>
         <source>Couldn&apos;t get to the bug reporting server.</source>
-        <translation type="unfinished"></translation>
+        <translation>Nemohl jsem se spojit se serverem na hlášení chyb.</translation>
     </message>
 </context>
 </TS>
